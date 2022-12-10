7109e47550e9555ea9fff7c5fc970f64db08552a
Nadir Fejzic <nadirfejzo@gmail.com>
2022-12-11 00:28:56 +0100

    feat: implement day 1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,11 +1,37 @@
 use anyhow::Result;
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 struct Elf {
     bag: Vec<usize>,
 }
 
+impl Elf {
+    fn cal_count(&self) -> usize {
+        self.bag.iter().sum()
+    }
+}
+
 fn main() -> Result<()> {
     let input = utils::read_file()?;
+
+    let mut iter = input.lines().peekable();
+
+    let mut elves: Vec<Elf> = Vec::new();
+
+    while iter.peek().is_some() {
+        let calories: Vec<usize> = iter
+            .by_ref()
+            .take_while(|line| !line.is_empty())
+            .map(|line| line.parse::<usize>())
+            .collect::<Result<_, _>>()?;
+
+        elves.push(Elf { bag: calories })
+    }
+
+    elves.sort_by_key(|elf1| elf1.cal_count());
+
+    let count: usize = elves.iter().rev().take(3).map(|elf| elf.cal_count()).sum();
+    println!("Total calories by top three elves: {count}");
 
     Ok(())
 }
